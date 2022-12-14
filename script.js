1c5db45b63eb1644d580380d191a45c9f703c250
gumlik <brianmikasa@gmail.com>
2022-12-13 18:47:07 -0800

    Create JS functionality and responsive grid size

--- script.js
+++ script.js
@@ -4,15 +4,30 @@ const container = document.createElement('div');
 container.classList.add('container');
 body.appendChild(container);
 
-let creatBigGrid = function () {
-  let pageLimit = 16 * 16;
+// let createBigGrid = function () {
+//   let pageLimit = prompt('Enter a number');
+//   pageLimit = Number(pageLimit) * pageLimit;
+//   for (let i = 0; i < pageLimit; i++) {
+//     let newDiv = document.createElement('div');
+//     newDiv.classList.add('box');
+//     container.appendChild(newDiv);
+//   }
+// };
+
+const createBigGrid = function () {
+  const userInput = Number(prompt('Enter a number'));
+  const pageLimit = Math.pow(userInput, 2);
   for (let i = 0; i < pageLimit; i++) {
-    let newDiv = document.createElement('div');
+    const newDiv = document.createElement('div');
     newDiv.classList.add('box');
     container.appendChild(newDiv);
   }
+  container.style.display = 'grid';
+  container.style.gridTemplateRows = `repeat(${userInput}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${userInput}, 1fr)`;
 };
-creatBigGrid();
+
+createBigGrid();
 
 const boxes = document.querySelectorAll('.box');
 
